140ee2dc22a78e3b3a76a14d9be0aa44186d631d
Robi <kevinucancme@gmail.com>
2025-01-10 00:06:20 +0530

    first commit

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'crypto-bank-rb',
-  location: 'us-central1'
-};
-
