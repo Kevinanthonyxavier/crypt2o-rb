71bb52c1b52f6a850005175165b01428b2cb2843
Robi <kevinucancme@gmail.com>
2025-01-09 23:21:45 +0530

    Fix critical issue

--- cr2/src/index.ts
+++ cr2/src/index.ts
@@ -7,8 +7,18 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+// If `onRequest` is unused, remove the import:
+// Remove the logger import if unused
+// import * as logger from "firebase-functions/logger";
+
+// If you plan to use `logger`, include an example usage like this:
 import * as logger from "firebase-functions/logger";
+import { onRequest } from "firebase-functions/v2/https";
+
+export const helloWorld = onRequest((req, res) => {
+  logger.info("Request received", { method: req.method, path: req.path }); // Log details from req
+  res.send("Hello, world!");
+});
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
